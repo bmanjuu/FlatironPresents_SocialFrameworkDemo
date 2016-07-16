71a69d97bf66c4d4ed1b954032509afe76971c92
bmanjuu <bettyofung@gmail.com>
2016-07-16 12:01:49 -0400

    installed Pinterest SDK/Cocoapod

---
+++ Pods/Target Support Files/Pods-FlatironPresents_SocialFrameworkDemo/Pods-FlatironPresents_SocialFrameworkDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlatironPresents_SocialFrameworkDemo : NSObject
+@end
+@implementation PodsDummy_Pods_FlatironPresents_SocialFrameworkDemo
+@end
